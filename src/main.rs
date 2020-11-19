27efd01db088f6f226777b49c45a44981d2a33d9
AN <hellnote01-github@yahoo.com>
2020-11-19 13:17:05 +0100

    reworking grammar

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-//use std::fs;
+use std::fs;
 
 extern crate pest;
 #[macro_use]
@@ -14,11 +14,7 @@ pub struct LaTeXParser;
 fn printout(tk: pest::iterators::Pair<Rule>, l: usize) {
     let spaces: String = " |  ".repeat(l);
     let rule_str = [spaces.clone(), "Rule: ".to_string()].join("");
-    let span_str = [spaces.clone(), "Span: ".to_string()].join("");
-    let text_str = [spaces.clone(), "Text: ".to_string()].join("");
-    println!("{}{:?}", rule_str, tk.as_rule());
-    println!("{}{:?}", span_str, tk.as_span());
-    println!("{}{:?}", text_str, tk.as_str());
+    println!("{}{:?}  {:?}", rule_str, tk.as_rule(), tk.as_str());
     for inner_tk in tk.into_inner() {
         printout(inner_tk, l+1);
     }
@@ -26,11 +22,9 @@ fn printout(tk: pest::iterators::Pair<Rule>, l: usize) {
 
 
 fn main() {
-    //let input = "../latex_test.tex";
-    //let latex = fs::read_to_string(input).expect("Cannot open file");
+    let input = "latex_test.tex";
+    let latex = fs::read_to_string(input).expect("Cannot open file");
 
-    let latex = "$$ i\\hbar\\partial_t\\psi = H \\psi $$";
-    let latex = "\\[ i\\hbar\\frac{\\partial\\psi}{\\partial t} = H \\psi \\]";
     let parse = LaTeXParser::parse(Rule::intro, &latex)
                             .expect("").next().unwrap();
 
